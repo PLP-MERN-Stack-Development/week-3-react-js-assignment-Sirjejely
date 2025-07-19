8fb1632b23cbb83c4f71f1bc2acf2eb2e965d797
Okoroego Chijioke Henry <chodasolid@gmail.com>
2025-07-19 13:26:45 +0100

    npm run build folder for netlify deployment

--- wk3-mern/src/components/Footer.jsx
+++ wk3-mern/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
-      <p>© 2025 ChoQuann It. All rights reserved.</p>
+      <p>© 2025 ChoQuann IT. All rights reserved.</p>
     </footer>
   );
 } 
